1e808c9c4422e2519cabaf18dd27208ca3963e1d
howie6879 <huyu@amz123.com>
2022-11-24 12:40:10 +0800

    Update config

--- lua/lsp/ultisnips.lua
+++ lua/lsp/ultisnips.lua
@@ -1,2 +1,5 @@
 -- python3 -m pip install --upgrade pynvim
-vim.g.UltiSnipsSnippetDirectories = {'ultisnips', 'ultisnips/my_snippets'}
+-- /usr/bin/python3 -m pip install pynvim
+vim.g.UltiSnipsSnippetDirectories = { 'ultisnips', 'ultisnips/my_snippets' }
+vim.g.python3_host_prog = '/usr/bin/python3'
+-- vim.g.loaded_python3_provider = 0
